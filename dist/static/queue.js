30a2ef795e8b7a7c2da3c014c5c1240301a21cd8
shaodahong <337097324@qq.com>
2017-03-24 23:30:46 +0800

    bug fix

---
+++ dist/static/queue.js
@@ -0,0 +1,25 @@
+webpackJsonp([4],{
+
+/***/ 17:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_vue___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_vue__);
+
+
+const name = 'queue';
+
+/***/ }),
+
+/***/ 66:
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+module.exports = __webpack_require__(17);
+
+
+/***/ })
+
+},[66]);
