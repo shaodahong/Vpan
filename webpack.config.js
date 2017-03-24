d6ec584b2ca30a5cebd9038fb57335bb839302e4
shaodahong <337097324@qq.com>
2017-03-24 23:54:27 +0800

    bug fix

---
+++ webpack.config.js
@@ -0,0 +1,157 @@
+var path = require('path');
+var fs = require("fs");
+var glob = require('glob');
+var Webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var isPro = process.env.NODE_ENV === 'production' ? true : false;
+
+var publicPath = 'http://localhost:8000/';
+
+isPro = false;
+
+var webpackConfig = {
+    entry: {},
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        publicPath: publicPath,
+        filename: isPro ? 'static/[name].[hash:5].js' : 'static/[name].js',
+    },
+    resolve: {
+        extensions: ['*', '.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.common.js'
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                loaders: {
+                    css: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader'
+                        ]
+                    }),
+                    scss: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader',
+                            'sass-loader'
+                        ]
+                    }),
+                }
+            }
+        }, {
+            test: /\.js/,
+            loader: 'babel-loader',
+            exclude: './node_modules/',
+            include: path.resolve(__dirname, './src')
+        },  {
+            test: /\.css/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader'
+                ]
+            }),
+            exclude: './node_modules/',
+            include: path.resolve(__dirname, './src')
+        }, {
+            test: /\.scss/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }),
+            exclude: './node_modules/',
+            include: path.resolve(__dirname, './src')
+        },{
+            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+            exclude: './node_modules/',
+            include: path.resolve(__dirname, './src'),
+            use: [{
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: 'static/images/[name].[hash:5].[ext]'
+                }
+            }, {
+                loader: 'image-webpack-loader',
+                options: {
+                    mozjpeg: {
+                        progressive: true,
+                    },
+                    gifsicle: {
+                        interlaced: false,
+                    },
+                    optipng: {
+                        optimizationLevel: 7,
+                    },
+                    pngquant: {
+                        quality: '60-80',
+                        speed: 4
+                    },
+                },
+            }],
+        }, {
+            test: /\.html$/,
+            loader: 'html-loader',
+            exclude: './node_modules/',
+            include: path.resolve(__dirname, './src')
+        }]
+    },
+    plugins: [
+        new Webpack.HotModuleReplacementPlugin(),
+        new Webpack.NoEmitOnErrorsPlugin(),
+        new Webpack.ProvidePlugin({}),
+        new Webpack.optimize.CommonsChunkPlugin({
+            name: 'vendors',
+        }),
+        new ExtractTextPlugin({
+            filename: isPro ? 'static/css/[name].[hash:5].css' : 'static/css/[name].css',
+            allChunks: true,
+            disable: false
+        }),
+    ],
+    devServer: {
+        historyApiFallback: {
+            disableDotRule: true
+        }
+    },
+}
+
+function getEntries(globPath) {
+    var files = glob.sync(globPath),
+        entries = {};
+
+    files.forEach(function(filepath) {
+        var split = filepath.split('/');
+        var name = split[split.length - 2];
+
+        entries[name] = filepath;
+    });
+
+    return entries;
+}
+
+var entries = getEntries('./src/pages/**/*.js');
+var hot = 'webpack-hot-middleware/client?reload=true';
+
+Object.keys(entries).forEach(function(name) {
+    webpackConfig.entry[name] = [hot, entries[name]];
+    var plugin = new HtmlWebpackPlugin({
+        filename: name + '.html',
+        template: './src/pages/' + name + '/' + name + '.html',
+        inject: true,
+        chunks: [name, 'vendors']
+    });
+    webpackConfig.plugins.push(plugin);
+})
+
+module.exports = webpackConfig;
---
+++ webpack.dev.js
@@ -0,0 +1,46 @@
+// server.js
+// 依赖项
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const history = require('connect-history-api-fallback');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+// const proxy = require('express-http-proxy');
+
+const config = require('./webpack.config.js');
+const isDeveloping = true;
+const port = isDeveloping ? 8000 : process.env.PORT;
+
+const app = express();
+
+if (isDeveloping) {
+    const compiler = webpack(config);
+    const middleware = webpackMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+        contentBase: config.output.path,
+        hot: true,
+	    stats: {
+	      colors: true,
+	      hash: false,
+	      timings: true,
+	      chunks: false,
+	      chunkModules: false,
+	      modules: false
+	    }
+    });
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    app.use(express(path.join(__dirname + './dist/')));
+    app.use(history());
+    // app.get('*', function response(req, res) {
+    //     res.sendFile(path.join(__dirname + '/server/view/index.html'));
+    // });
+}
+
+app.listen(port, function onStart(err) {
+    if (err) {
+        console.log(err);
+    }
+}
+)
